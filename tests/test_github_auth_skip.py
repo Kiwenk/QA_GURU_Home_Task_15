43210da38596b60a75df0d3b4e37d724d43d8c40
Katsuba Igor <kasubaigor@gmail.com>
2024-08-05 19:46:24 +0300

    1

--- tests/test_github_auth_skip.py
+++ tests/test_github_auth_skip.py
@@ -7,11 +7,11 @@ def test_mobile_skip(setup_browser):
     if setup_browser == "mobile":
         pytest.skip("Это мобильное разрешение")
     base_actions.open_git()
-    base_actions.mobile_actions()
+    base_actions.desktop_actions()
 
 
 def test_desktop_skip(setup_browser):
     if setup_browser == "desktop":
         pytest.skip("Это десктопное разрешение")
     base_actions.open_git()
-    base_actions.desktop_actions()
+    base_actions.mobile_actions()
